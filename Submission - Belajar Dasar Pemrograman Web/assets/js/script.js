3f073307ed3eb912085f555531295e7b9dfd4564
Dev Project <atagnan98@gmail.com>
2022-07-14 02:13:46 +0700

    menambahkan folder belajar dasar pemrograman web

---
+++ Submission - Belajar Dasar Pemrograman Web/assets/js/script.js
@@ -0,0 +1,8 @@
+console.log("Javascript berhasil dijalankan");
+
+document.getElementById('name').innerHTML = 'Devis Destian Nugraha';
+document.getElementById('date').innerHTML = '01 Desember 1999';
+document.getElementById('age').innerHTML = '21 Tahun';
+document.getElementById('city').innerHTML = 'Kota Garut';
+document.getElementById('religion').innerHTML = 'Islam';
+document.getElementById('graduate').innerHTML = 'SMA Negeri 6 Garut';
